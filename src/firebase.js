f2eb6aa53f13ce17e178d1aff45868df62158097
zulhilmiahmad1 <mzulhilmi.ahmad@gmail.com>
2021-01-03 17:39:01 +1100

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCvHlZ8PMthWjUw27ovFvY1j5P9vrmwyUU",
+  authDomain: "clone-f0dfb.firebaseapp.com",
+  projectId: "clone-f0dfb",
+  storageBucket: "clone-f0dfb.appspot.com",
+  messagingSenderId: "317050028406",
+  appId: "1:317050028406:web:a94d668acf33ca9da9fa57",
+  measurementId: "G-KRD8T5CE8W",
+};
