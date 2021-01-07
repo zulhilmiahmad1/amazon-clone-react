028081e4460ec66f99b72b08b047985c9be16303
zulhilmiahmad1 <mzulhilmi.ahmad@gmail.com>
2021-01-08 08:38:38 +1100

    adding new componet

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCvHlZ8PMthWjUw27ovFvY1j5P9vrmwyUU",
   authDomain: "clone-f0dfb.firebaseapp.com",
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: "1:317050028406:web:a94d668acf33ca9da9fa57",
   measurementId: "G-KRD8T5CE8W",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
